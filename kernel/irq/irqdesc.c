a207331eb3c4aa976f84a9fe0ca0aa9401fa4806
blueseaxy <workingn6@gmail.com>
2023-10-24 10:18:08 +0000

    Revert "kernel: Introduce SBalance IRQ balancer"
    This reverts commit d123093a6dce7c40ad1593db5166828541e92cf5.

--- kernel/irq/irqdesc.c
+++ kernel/irq/irqdesc.c
@@ -161,10 +161,7 @@ static struct irq_desc *alloc_desc(int irq, int node, struct module *owner)
 	raw_spin_lock_init(&desc->lock);
 	lockdep_set_class(&desc->lock, &irq_desc_lock_class);
 
-	desc_set_defaults(irq, desc, node, affinity, owner);
-	irqd_set(&desc->irq_data, flags);
-	kobject_init(&desc->kobj, &irq_kobj_type);
-	sbalance_desc_add(desc);
+	desc_set_defaults(irq, desc, node, owner);
 
 	return desc;
 
@@ -179,8 +176,6 @@ static void free_desc(unsigned int irq)
 {
 	struct irq_desc *desc = irq_to_desc(irq);
 
-	sbalance_desc_del(desc);
-	irq_remove_debugfs_entry(desc);
 	unregister_irq_proc(irq, desc);
 
 	/*
